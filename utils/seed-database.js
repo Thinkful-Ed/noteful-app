8678e5c9b01c7af671d8363ce7b91406aa9894e2
Chris Klanac <cklanac@users.noreply.github.com>
2018-04-19 16:22:08 -0400

    fix: hash passwords upon seed

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -13,21 +13,26 @@ const seedFolders = require('../db/seed/folders');
 const seedTags = require('../db/seed/tags');
 const seedUsers = require('../db/seed/users');
 
+console.log(`Connecting to mongodb at ${MONGODB_URI}`);
 mongoose.connect(MONGODB_URI)
-  .then(() => mongoose.connection.db.dropDatabase())
   .then(() => {
+    console.log('Dropping database');
+    return mongoose.connection.db.dropDatabase();
+  })
+  .then(() => {
+    console.log('Seeding database');
     return Promise.all([
       Note.insertMany(seedNotes),
       Folder.insertMany(seedFolders),
       Folder.createIndexes(),
       Tag.insertMany(seedTags),
       Tag.createIndexes(),
-      User.insertMany(seedUsers),
+      // `.create()` calls mongoose pre save middleware
+      User.create(seedUsers),
       User.createIndexes(),
     ]);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
-    console.error(`ERROR: ${err.message}`);
     console.error(err);
   });
