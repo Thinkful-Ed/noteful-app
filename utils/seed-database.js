9e0b8b14b9c71297a21c7cb7ab0dd42193d87cb0
Chris Klanac <cklanac@users.noreply.github.com>
2018-04-19 15:49:49 -0400

    feat: add seed database util

---
+++ utils/seed-database.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const { MONGODB_URI } = require('../config');
+const Note = require('../models/note');
+const Folder = require('../models/folder');
+const Tag = require('../models/tag');
+const User = require('../models/user');
+
+const seedNotes = require('../db/seed/notes');
+const seedFolders = require('../db/seed/folders');
+const seedTags = require('../db/seed/tags');
+const seedUsers = require('../db/seed/users');
+
+mongoose.connect(MONGODB_URI)
+  .then(() => mongoose.connection.db.dropDatabase())
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(seedFolders),
+      Folder.createIndexes(),
+      Tag.insertMany(seedTags),
+      Tag.createIndexes(),
+      User.insertMany(seedUsers),
+      User.createIndexes(),
+    ]);
+  })
+  .then(() => mongoose.disconnect())
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
