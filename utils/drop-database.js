c2c362fc774a345c12b41b7b04952304ee3c8b22
Chris Klanac <cklanac@users.noreply.github.com>
2018-04-19 16:22:26 -0400

    feat: add drop database util

---
+++ utils/drop-database.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const { MONGODB_URI } = require('../config');
+
+console.log(`Connecting to mongodb at ${MONGODB_URI}`);
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('Dropping database');
+    return mongoose.connection.db.dropDatabase();
+  })
+  .then(() => mongoose.disconnect())
+  .catch(err => {
+    console.error(err);
+  });
